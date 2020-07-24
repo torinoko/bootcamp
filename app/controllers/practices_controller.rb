cff60bf5bdc76b918050489319fc21d1f9ed94e7
kota kanazawa <kota0818@gmail.com>
2020-07-24 18:31:41 +0900

    処理を共通化

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -23,6 +23,7 @@ class PracticesController < ApplicationController
       SlackNotification.notify "<#{url_for(current_user)}|#{current_user.login_name}>が<#{url_for(@practice)}|#{@practice.title}>を作成しました。",
                                username: "#{current_user.login_name}@bootcamp.fjord.jp",
                                icon_url: current_user.avatar_url
+      save_ref_books(@practice)
       redirect_to @practice, notice: 'プラクティスを作成しました。'
     else
       render :new
@@ -46,6 +47,7 @@ class PracticesController < ApplicationController
         book.save
       end
 
+      save_ref_books(@practice)
       redirect_to @practice, notice: "プラクティスを更新しました。"
     else
       render :edit
@@ -77,15 +79,24 @@ class PracticesController < ApplicationController
     @course = Course.find(params[:course_id]) if params[:course_id]
   end
 
-  def book_search(asin)
-    request = Vacuum.new(marketplace: 'JP',
-      access_key: 'AKIAJM4IHXPFQ67OSZCA',
-      secret_key: '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',
-      partner_tag: 'twitter0f1-22')
+    def book_search(asin)
+      request = Vacuum.new(marketplace: 'JP',
+        access_key: 'AKIAJM4IHXPFQ67OSZCA',
+        secret_key: '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',
+        partner_tag: 'twitter0f1-22')
 
-    response = request.get_items(
-    item_ids: [asin],
-    resources: ['Images.Primary.Small', 'ItemInfo.Title', 'ItemInfo.Features', 'Offers.Summaries.HighestPrice', 'ParentASIN']
-    )
-  end
+      response = request.get_items(
+      item_ids: [asin],
+      resources: ['Images.Primary.Small', 'ItemInfo.Title', 'ItemInfo.Features', 'Offers.Summaries.HighestPrice', 'ParentASIN']
+      )
+    end
+
+    def save_ref_books(practice)
+      practice.reference_books.each do |book|
+        res = book_search(book.asin)
+        book.page_url = res.dig('ItemsResult', 'Items')[0]['DetailPageURL']
+        book.image_url = res.dig('ItemsResult', 'Items')[0]['Images']['Primary']['Small']['URL']
+        book.save
+      end
+    end
 end
