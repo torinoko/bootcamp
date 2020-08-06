7faa8ce9a07c514462d5db52ae670ede28af0a91
kota kanazawa <kota0818@gmail.com>
2020-08-06 12:00:28 +0900

    vcr gemを削除

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,16 +12,6 @@ require 'vcr'
 WebMock.allow_net_connect!
 Webdrivers.cache_time = 86_400
 
-VCR.configure do |config|
-  config.cassette_library_dir = "test/vcr_cassettes"
-  config.hook_into :webmock
-  config.ignore_localhost = true
-  config.default_cassette_options = {
-    record: :new_episodes
-  }
-  config.allow_http_connections_when_no_cassette = true
-end
-
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   # parallelize(workers: :number_of_processors)
